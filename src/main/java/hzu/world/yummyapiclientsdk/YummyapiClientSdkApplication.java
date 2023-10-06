d1ff44ffaf071b8a736f3bc15e88e0eaa61e4147
a52doraemon <chenyangmang@outlook.com>
2023-10-06 23:53:32 +0800

    1、初始化工程，maven，springboot 2.7.16 2、初始化依赖lombok，spring configuration processor（帮助开发者自动生成代码补全）

---
+++ src/main/java/hzu/world/yummyapiclientsdk/YummyapiClientSdkApplication.java
@@ -0,0 +1,13 @@
+package hzu.world.yummyapiclientsdk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YummyapiClientSdkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YummyapiClientSdkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hzu/world/yummyapiclientsdk/YummyapiClientSdkApplicationTests.java
@@ -0,0 +1,13 @@
+package hzu.world.yummyapiclientsdk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YummyapiClientSdkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
